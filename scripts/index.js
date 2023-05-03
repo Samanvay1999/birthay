533563eef6025ea33bd8a9cbf0a237165ad29be3
Samanvay Singh Thakur <95512544+Samanvay1999@users.noreply.github.com>
2023-05-03 13:17:08 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'may 03, 2023',
+  birthdate: 'may 26, 2023',
   name: 'my dear Bachha swearheart'
 };
 
